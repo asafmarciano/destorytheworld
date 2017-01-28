1547cf3da3234cd7d2cf412db655e0e1fac3efd4
marciano <asafmarciano@gmail.com>
2017-01-29 00:52:05 +0200

    commit test

--- DestroyTheWorld/ViewController.swift
+++ DestroyTheWorld/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("aaa")
+        print("bbb")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
